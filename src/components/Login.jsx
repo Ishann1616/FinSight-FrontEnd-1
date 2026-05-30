4fa4edb52d4b902e42f46a15e8b7289556f725f6
Ishan Dewangan <ishan.dewangan2005@gmail.com>
2026-05-31 01:05:48 +0530

    React Router setup + Login working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,28 @@
-import { userState} from 'react'
+import { useState} from 'react'
 
 function Login(){
-    const[email, setEmail]= userState('')
-    const[password, setPassword]= userState('')
+    const[email, setEmail]= useState('')
+    const[password, setPassword]= useState('')
 
     return(
         <div>
             <h2>Login</h2>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
         </div>
+
+
+
     )
 }
 
