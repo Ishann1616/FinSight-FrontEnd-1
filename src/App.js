a8ec3d65356495ee0c110448f5df7d58caab74c1
Ishan Dewangan <ishan.dewangan2005@gmail.com>
2026-06-02 10:14:00 +0530

    Dashboard component + routing working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard"
 import Login from "./components/Login"
 
 function App(){
@@ -6,6 +7,7 @@ function App(){
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
